f35b075cf4736ebe11ce8d86bb773f2f3cea95aa
Peter van Galen <petervangalen@gmail.com>
2019-07-11 06:34:04 -0400

    Update BarcodeSimilarity.R

--- BarcodeSimilarity.R
+++ BarcodeSimilarity.R
@@ -1,4 +1,4 @@
-# Peter van Galen, 190312
+# Peter van Galen, 190710
 # This script generates a matrix where the color indicates the number of differences between molecular barcodes.
 
 # Load packages
@@ -9,19 +9,20 @@ library(gplots)
 rm(list=ls())
 
 # Load data: refer to a .txt file that contains barcodes, one per line. All barcodes should be the same length.
-table1 <- read.table("/Users/vangalen/Desktop/bcs.txt", sep = "\t", na.strings = "NA", header = F)
+barcodes.df <- read.table("/Users/vangalen/Desktop/bcs.txt", sep = "\t", na.strings = "NA", header = F)
 
 # Calculate differences in barcodes
-ind.mat <- expand.grid(1:nrow(table1),1:nrow(table1))
-hamming.mat <- matrix( apply(ind.mat,1,function(x,t1) stringdist(t1[x[1],1], t1[x[2],1], method = c("hamming"), useBytes = F),table1), ncol = nrow( table1 ), nrow = nrow( table1 ) )
-colnames(hamming.mat) <- table1[,1]
-rownames(hamming.mat) <- table1[,1]
+ind.mat <- expand.grid(1:nrow(barcodes.df),1:nrow(barcodes.df))
+hamming.mat <- matrix( apply(ind.mat,1,function(x,t1) stringdist(t1[x[1],1], t1[x[2],1], method = c("hamming"), useBytes = F),barcodes.df), ncol = nrow( barcodes.df ), nrow = nrow( barcodes.df ) )
+colnames(hamming.mat) <- barcodes.df[,1]
+rownames(hamming.mat) <- barcodes.df[,1]
 
 # Plot
 mycol <- c("darkred", "darkred", "darkred", "orangered", "orangered", "darkgreen", "darkgreen", "forestgreen", "forestgreen", rep("limegreen", (max(hamming.mat)*2-9)))
-heatmap.2(hamming.mat, Rowv = F, Colv = F, dendrogram = "none", col = mycol, trace =  "none", margins = c(10,10), density.info = "none")
+heatmap.2(hamming.mat, Rowv = F, Colv = F, dendrogram = "none", col = mycol, trace =  "none", margins = c(10,10), density.info = "none", key.xlab = "Hamming distance")
 
-# To save a pdf file:
-#pdf(file = "/Users/vangalen/Desktop/hamming.pdf", height = 6, width = 6)
-#heatmap.2(hamming.mat, Rowv = F, Colv = F, dendrogram = "none", col = mycol, trace =  "none", margins = c(10,10), density.info = "none")
+# Optional: save an image
+#pdf(file = "/Users/vangalen/Desktop/hamming.pdf", height = 6, width = 6) # or
+#png(file = "/Users/vangalen/Desktop/hamming.png", units = "px", height = 1800, width = 1800, res = 300)
+#heatmap.2(hamming.mat, Rowv = F, Colv = F, dendrogram = "none", col = mycol, trace =  "none", margins = c(10,10), density.info = "none", key.xlab = "Hamming distance")
 #dev.off()
